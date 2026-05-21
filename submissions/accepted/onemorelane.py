f61660062052a72c000e6175b57baa042a8479ba
Markus Sværke Staael <markus@staael.com>
2026-05-21 21:20:28 +0200

    Name fix and usage of ford fulkerson

--- submissions/accepted/onemorelane.py
+++ submissions/accepted/onemorelane.py
@@ -4,6 +4,7 @@ from collections import deque
 input = sys.stdin.readline
 INF = 10**18
 
+
 def build_graph(n, edges, upgrade_idx=-1):
     g = [[] for _ in range(n)]
 
@@ -13,68 +14,48 @@ def build_graph(n, edges, upgrade_idx=-1):
         fwd = len(g[u])
         bwd = len(g[v])
 
+        # store: [to, capacity, reverse_index]
         g[u].append([v, cap, bwd])
         g[v].append([u, 0, fwd])
 
     return g
 
-def bfs_level(g, s, t, level):
-    level[:] = [-1] * len(level)
-    level[s] = 0
 
-    q = deque([s])
-
-    while q:
-        u = q.popleft()
-
-        for v, cap, _ in g[u]:
-            if cap > 0 and level[v] < 0:
-                level[v] = level[u] + 1
-                q.append(v)
-
-    return level[t] >= 0
-
-def dfs_block(g, u, t, pushed, level, it):
+def dfs_augment(g, u, t, f, vis):
     if u == t:
-        return pushed
+        return f
 
-    while it[u] < len(g[u]):
-        v, cap, rev = g[u][it[u]]
+    vis.add(u)
 
-        if cap > 0 and level[v] == level[u] + 1:
-            d = dfs_block(g, v, t, min(pushed, cap), level, it)
+    for i, (v, cap, rev) in enumerate(g[u]):
+        if cap > 0 and v not in vis:
+            pushed = dfs_augment(g, v, t, min(f, cap), vis)
 
-            if d > 0:
-                g[u][it[u]][1] -= d
-                g[v][rev][1] += d
-                return d
-
-        it[u] += 1
+            if pushed > 0:
+                g[u][i][1] -= pushed
+                g[v][rev][1] += pushed
+                return pushed
 
     return 0
 
-def dinic(g, s, t):
-    n = len(g)
 
-    level = [-1] * n
+def ford_fulkerson(g, s, t):
     flow = 0
 
-    while bfs_level(g, s, t, level):
-        it = [0] * n
+    while True:
+        vis = set()
+        pushed = dfs_augment(g, s, t, INF, vis)
 
-        while True:
-            f = dfs_block(g, s, t, INF, level, it)
+        if pushed == 0:
+            break
 
-            if f == 0:
-                break
-
-            flow += f
+        flow += pushed
 
     return flow
 
+
 def reachable(g, s):
     vis = set([s])
-
     q = deque([s])
 
     while q:
@@ -87,25 +68,24 @@ def reachable(g, s):
 
     return vis
 
+
 def solve():
     sys.setrecursionlimit(200000)
 
     n, m = map(int, input().split())
 
     edges = []
-
     for _ in range(m):
         u, v, c = map(int, input().split())
         edges.append((u, v, c))
 
-    # Original max flow
+    # Base max flow
     g = build_graph(n, edges)
-    base_flow = dinic(g, 0, n - 1)
+    base_flow = ford_fulkerson(g, 0, n - 1)
 
-    # Find source side of min cut
+    # Min cut source side
     S = reachable(g, 0)
 
-    # Candidate edges in min cut
     cut_edges = [
         i for i, (u, v, c) in enumerate(edges)
         if u in S and v not in S
@@ -114,11 +94,9 @@ def solve():
     best_flow = base_flow
     best_edge = (edges[0][0], edges[0][1])
 
-    # Try upgrading each cut edge
     for i in cut_edges:
         g2 = build_graph(n, edges, upgrade_idx=i)
-
-        new_flow = dinic(g2, 0, n - 1)
+        new_flow = ford_fulkerson(g2, 0, n - 1)
 
         if new_flow > best_flow:
             best_flow = new_flow
@@ -126,4 +104,6 @@ def solve():
 
     print(best_edge[0], best_edge[1])
 
-solve()
+
+if __name__ == "__main__":
+    solve()
